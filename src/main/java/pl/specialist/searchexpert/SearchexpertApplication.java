bba5c8a8f17a709ac63f06bfe4e92fd9fdd76472
Enigma <enigma3420@gmail.com>
2020-04-10 15:48:36 +0200

    Create Develop branch

---
+++ src/main/java/pl/specialist/searchexpert/SearchexpertApplication.java
@@ -0,0 +1,13 @@
+package pl.specialist.searchexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchexpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchexpertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/specialist/searchexpert/SearchexpertApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.specialist.searchexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchexpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
