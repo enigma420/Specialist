f4152e82bea4ecd4ec7c8088574a9e806357691e
Enigma <enigma3420@gmail.com>
2020-04-12 18:45:07 +0200

    Create Specialist Entity and Repository with methods to find/delete/save specialist by various fields and mixed. Moreover implement unit test to SpecialistRepository for these methods. (FINAL STEP)

--- src/main/java/pl/specialist/searchexpert/repositories/SpecialistRepo.java
+++ src/main/java/pl/specialist/searchexpert/repositories/SpecialistRepo.java
@@ -1,24 +1,24 @@
 package pl.specialist.searchexpert.repositories;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.specialist.searchexpert.domains.specialist.Province;
 import pl.specialist.searchexpert.domains.specialist.Specialist;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface SpecialistRepo extends JpaRepository<Specialist,Long> {
+
+    @Override
+    long count();
 
     @Override
     <S extends Specialist> S save(S s);
 
     @Override
     <S extends Specialist> List<S> saveAll(Iterable<S> iterable);
-
-    @Override
-    Optional<Specialist> findById(Long aLong);
 
     @Override
     List<Specialist> findAll();
@@ -29,32 +29,22 @@ public interface SpecialistRepo extends JpaRepository<Specialist,Long> {
     @Override
     void deleteAll();
 
+    Specialist findSpecialistById(Long id);
 
-
-    /*Find SPECIALIST by location*/
-    Specialist findSpecialistByProvince(Province province);
-    Specialist findSpecialistByCity(String city);
-    Specialist findSpecialistByProvinceAndCity(Province province, String city);
-
-    /*Find SPECIALIST by name and surname*/
-    Specialist findSpecialistByNameAndSurname(String name,String surname);
-
-    /*Find SPECIALIST by profession*/
-    Specialist findSpecialistByProfession(String profession);
-
-    /*MIXED*/
-    /*Find SPECIALIST by location and profession*/
-    Specialist findSpecialistByProvinceAndProfession(Province province, String profession);
-    Specialist findSpecialistByCityAndProfession(String city, String profession);
-    Specialist findSpecialistByProvinceAndCityAndProfession(Province province,String city,String profession);
-
-
-
+    /*Find SPECIALISTS by name and surname*/
+    Iterable<Specialist> findSpecialistsByNameAndSurname(String name,String surname);
 
     /*Find SPECIALISTS by location*/
     Iterable<Specialist> findSpecialistsByProvince(Province province);
     Iterable<Specialist> findSpecialistsByCity(String city);
+    Iterable<Specialist> findSpecialistsByProvinceAndCity(Province province, String city);
 
     /*Find SPECIALISTS by profession*/
     Iterable<Specialist> findSpecialistsByProfession(String profession);
+
+    /*MIXED*/
+    /*Find SPECIALIST by location and profession*/
+    Iterable<Specialist> findSpecialistsByProvinceAndProfession(Province province, String profession);
+    Iterable<Specialist> findSpecialistsByCityAndProfession(String city, String profession);
+    Iterable<Specialist> findSpecialistsByProvinceAndCityAndProfession(Province province,String city,String profession);
 }
